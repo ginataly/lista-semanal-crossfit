01b8cbe2c6f892b4429c16ce04ef4cd795a981e6
Giovanna Nataly dos Santos Garcia <g.nataly@btp.com.br>
2024-01-31 11:47:41 -0300

    add style para o navegador não criar o próprio style

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function alterarStatus(id) {
+    let botaoClicado = document.getElementById(`exercicio-${id}`);
+    let imagem = botaoClicado.querySelector('.dashboard__item__img');
+    let botao = botaoClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = botaoClicado.querySelector('.dashboard__item__name');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Feito';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Refazer';
+        botao.classList.add('dashboard__item__button--return');
+    }
+
+}
