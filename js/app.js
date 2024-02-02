b26b67016f8a0a07180382d538cc51c517e73d1b
Giovanna Nataly dos Santos Garcia <g.nataly@btp.com.br>
2024-02-02 14:35:56 -0300

    stylesheet correto agora

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function alterarStatus(id) {
     let botaoClicado = document.getElementById(`exercicio-${id}`);
     let imagem = botaoClicado.querySelector('.dashboard__item__img');
     let botao = botaoClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = botaoClicado.querySelector('.dashboard__item__name');
+    let exercicio = botaoClicado.querySelector('.dashboard__item__name');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented');
